1cd4c6f9e6781e7ca06b38e92a92204635b0518a
Asley10A <135353511+Asley10A@users.noreply.github.com>
2023-07-04 20:43:24 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,72 @@
+def on_received_number(receivedNumber):
+    global dato
+    dato = receivedNumber
+radio.on_received_number(on_received_number)
+
+dato = 0
+dato = 4
+radio.set_group(27)
+basic.show_icon(IconNames.HAPPY)
+
+def on_forever():
+    if dato == 0:
+        pins.digital_write_pin(DigitalPin.P0, 1)
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P2, 1)
+        pins.digital_write_pin(DigitalPin.P13, 0)
+        basic.show_leds("""
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            """)
+    elif dato == 1:
+        pins.digital_write_pin(DigitalPin.P0, 0)
+        pins.digital_write_pin(DigitalPin.P1, 1)
+        pins.digital_write_pin(DigitalPin.P2, 0)
+        pins.digital_write_pin(DigitalPin.P13, 1)
+        basic.show_leds("""
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            """)
+    elif dato == 2:
+        pins.digital_write_pin(DigitalPin.P0, 0)
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P2, 1)
+        pins.digital_write_pin(DigitalPin.P13, 0)
+        basic.show_leds("""
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            """)
+    elif dato == 3:
+        pins.digital_write_pin(DigitalPin.P0, 1)
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P2, 0)
+        pins.digital_write_pin(DigitalPin.P13, 0)
+        basic.show_leds("""
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            """)
+    else:
+        pins.digital_write_pin(DigitalPin.P0, 0)
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P2, 0)
+        pins.digital_write_pin(DigitalPin.P13, 0)
+        basic.show_leds("""
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            """)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,70 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    dato = receivedNumber
+})
+let dato = 0
+dato = 4
+radio.setGroup(3)
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    if (dato == 0) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (dato == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (dato == 2) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (dato == 3) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
